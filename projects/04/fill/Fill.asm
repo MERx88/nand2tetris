059e1be5c6a18a9ec4d118127edff87cd44dc35c
MER <wjang07@gmail.com>
2024-01-28 01:04:11 +0900

    Complete project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,39 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(KEY)
+@SCREEN
+D=A
+@screenPixel
+M=D
+@KBD
+D=M
+@BLACK
+D;JGT
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen
-// by writing 'black' in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen by writing
-// 'white' in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(WHITE)
+@screenPixel
+A=M
+M=0
+@screenPixel
+M=M+1
+D=M
+@24576
+D=A-D
+@WHITE
+D;JGT
+@KEY
+0;JMP
 
-//// Replace this comment with your code.
+(BLACK)
+@screenPixel
+A=M
+M=-1
+@screenPixel
+M=M+1
+D=M
+@24576
+D=A-D
+@BLACK
+D;JGT
+@KEY
+0;JMP
+
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -3,7 +3,32 @@
 
 //RAM[0]
 
-@R0
-D=M
-@R1
-D=D+D
+    @i
+    M=1
+    @sum
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R1                                                                       
+    D=D-M
+    @STOP
+    D;JGT
+    @sum
+    D=M
+    @R0
+    D=D+M
+    @sum
+    M=D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(STOP)
+    @sum
+    D=M
+    @R2
+    M=D
+(END)
+    @END
+    0;JMP
